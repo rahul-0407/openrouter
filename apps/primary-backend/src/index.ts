606d401b8df4405c4ca93af4b82532b67e4ff615
rahul-0407 <rahulkrgupta0407@gmail.com>
2026-02-21 18:45:38 +0530

    primary-backend completed

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,8 +1,15 @@
 import { Elysia } from "elysia";
-import {app as authApp} from "./modules/auth"
-import {app as apiKey} from "./modules/apiKeys"
+import { app as authApp } from "./modules/auth";
+import { app as apiKeyApp } from "./modules/apiKeys";
+import { app as modelsApp } from "./modules/models";
+import { app as paymentsApp } from "./modules/payments";
 
-const app = new Elysia().use(authApp).use(apiKey).listen(3000);
+const app = new Elysia()
+  .use(authApp)
+  .use(apiKeyApp)
+  .use(modelsApp)
+  .use(paymentsApp)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
