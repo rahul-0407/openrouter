0389bbbb55dc7fdf5f782023a92583932e7bf21f
rahul-0407 <rahulkrgupta0407@gmail.com>
2026-02-21 17:49:33 +0530

    apiKey completed

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,7 +1,8 @@
 import { Elysia } from "elysia";
 import {app as authApp} from "./modules/auth"
+import {app as apiKey} from "./modules/apiKeys"
 
-const app = new Elysia().use(authApp).listen(3000);
+const app = new Elysia().use(authApp).use(apiKey).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- apps/primary-backend/src/modules/apiKeys/models.ts
+++ apps/primary-backend/src/modules/apiKeys/models.ts
@@ -31,7 +31,7 @@ export namespace ApiKeyModel {
         message: t.Literal("Updating api key unsuccessful")
     })
 
-    export type disableApiKeyResponseFailedSchema = typeof disableApiKeyResponseSchema.static;
+    export type disableApiKeyResponseFailedSchema = typeof disableApiKeyResponseFailedSchema.static;
 
     export const getApiKeysResponseSchema = t.Object({
         apiKeys: t.Array(t.Object({
